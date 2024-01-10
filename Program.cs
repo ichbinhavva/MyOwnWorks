c0bbfd5ad314cb54e402402e53ae8b68050867dd
ichbinhavva <147437523+ichbinhavva@users.noreply.github.com>
2024-01-10 23:54:30 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,67 @@ namespace MyOwnWorks
     {
         static void Main(string[] args)
         {
+            
+            
+            
+            
+            
+            
+            
+            // ÖDEV
+ Console.WriteLine("Kaç tane sayı sıralamak istiyorsunuz?");
+ int count = int.Parse(Console.ReadLine());
+ Console.WriteLine("Başlangıç numarası giriniz:");
+ int start = int.Parse(Console.ReadLine());
+ Console.WriteLine("Bitiş numarası giriniz:");
+ int end = int.Parse(Console.ReadLine());
+ Console.WriteLine("Rastgele sayılar oluşturuldu:");
+
+ Random rnd = new Random();
+ int[] numbers = new int[count];
+
+ for (int i = 0; i < numbers.Length; i++)
+ {
+     int number;
+     bool unique;//sayıların benzersiz olup olmadığını kontrol eder.
+     do
+     {
+         number = rnd.Next(start, end);
+         unique = true; // atanan sayıların benzersizliğini doğrular.
+         for (int j = 0; j < i; j++)// daha önce üretilen sayıları karşılaştırmak için
+         {
+             if (numbers[j] == number)
+             {
+                 unique = false;
+                 break;
+             }
+         }
+     } while (!unique);
+     numbers[i] = number;
+ }
+ Array.Sort(numbers);
+ Console.WriteLine("Sıralı sayıları yazdır:");
+ for (int i = 0; i < numbers.Length; i++)
+ {
+     Console.WriteLine(numbers[i]);
+ }
+
+            
+               
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+            
             Random rnd = new Random();
             int rastgelesayi = rnd.Next(10, 30);
             Console.WriteLine("Üretilen sayı:{0}", rastgelesayi);
@@ -45,6 +106,10 @@ namespace MyOwnWorks
                 }
             }
             Console.ReadKey();
+
+
+
+            
 
 
         }
